3f14a6bfe22a2df3a6e531caf8bd77ded4b6e828
TalentUnlimited <122269523+TalentUnlimited@users.noreply.github.com>
2024-03-23 21:50:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,14 +4,13 @@ app = Flask(__name__)
 
 @app.route('/process-data', methods=['GET','POST'])
 def process_data():
-	return render_template("index.html")
-
-@app.route('/post-data', methods=['GET','POST'])
-def post_data():
-	data = request.form['input-data']  # Assuming JSON data is sent from frontend
-	# Process the data (e.g., perform some computation)
-	
-	return f"<p>{data}</p>"
+    if request.method == "GET":
+        return render_template("index.html")
+    else:
+        data = request.form['input-data']  # Assuming JSON data is sent from frontend
+        # Process the data (e.g., perform some computation)
+    
+        return f"<p>{data}</p>"    
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
